e9ee68007c75c1200bb9c4d01da4c5dd6c74101e
Roger Sicart <roger@adserverpub.com>
2015-11-19 18:07:59 +0100

    Tests for publisher site: user accepts all cookies

--- run.js
+++ run.js
@@ -81,9 +81,13 @@ casper.test.begin('user_accepts_all | all_is_empty', 3, function suite(test) {
 		/*
 		 * assertions
 		 */
-		test.assert(devicesFirst && true);
-		test.assert(devicesThird && true);
-		test.assertEquals(String(devicesFirst.shift()), String(devicesThird.shift()));
+		test.assert(!!devicesFirst);
+		casper.log('First: ' + devicesFirst.join(), 'debug');
+
+		test.assert(!!devicesThird);
+		casper.log('Third: ' + devicesThird.join(), 'debug');
+
+		test.assertEquals(devicesFirst[0], devicesThird[0]);
 
     });
 
@@ -93,13 +97,16 @@ casper.test.begin('user_accepts_all | all_is_empty', 3, function suite(test) {
 });
 
 
-casper.test.begin('user_accepts_all | cookie_third_is_empty', 3, function suite(test) {
-
-	var targetUrl = "http://publisher.localhost/publisher.html";
-	var firstDomain = "publisher.localhost";
-	var thirdDomain = ".adsp.localhost";
+casper.test.begin('user_accepts_all | cookie_third_is_empty', 4, function suite(test) {
 
 	phantom.clearCookies();
+
+	var deviceIdA = '1447859209.5be2fe18-5554-4378-42d2-7488f6ed93e1';
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls=1447859209770|v=1|di=' + deviceIdA,
+		'domain': firstDomain,
+	});
 
     casper.start(targetUrl, function() {
     });
@@ -121,9 +128,170 @@ casper.test.begin('user_accepts_all | cookie_third_is_empty', 3, function suite(
 		/*
 		 * assertions
 		 */
-		test.assert(devicesFirst && true);
-		test.assert(devicesThird && true);
-		test.assertEquals(String(devicesFirst.shift()), String(devicesThird.shift()));
+		test.assert(!!devicesFirst);
+		casper.log('First: ' + devicesFirst.join(), 'debug');
+
+		test.assert(!!devicesThird);
+		casper.log('Third: ' + devicesThird.join(), 'debug');
+
+		test.assertEquals(deviceIdA, devicesThird[0]); // original deviceId is kept
+		test.assertEquals(devicesFirst[0], devicesThird[0]);
+    });
+
+	casper.run(function() {
+		test.done();
+	});
+});
+
+
+casper.test.begin('user_accepts_all | cookie_first_is_empty', 4, function suite(test) {
+
+	phantom.clearCookies();
+
+	var deviceIdA = '1447859209.5be2fe18-5554-4378-42d2-7488f6ed93e1';
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls%3D1447859209000%7Cv%3D1%7Cdi%3D' + deviceIdA,
+		'domain': thirdDomain,
+	});
+
+    casper.start(targetUrl, function() {
+    });
+
+    casper.then(function() {
+		/*
+		 * catch cookies
+		 */
+		var cookies_by_domain = getCookiesByDomain(phantom.cookies);
+
+		// set regexps
+		var reFirst = /[0-9lsv|=]*(di=[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+		var reThird = /.*(di%3D[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+
+		// get device ids
+		var devicesFirst = getDeviceIdsByDomain(reFirst, '=', cookies_by_domain[firstDomain]);
+		var devicesThird = getDeviceIdsByDomain(reThird, '%3D', cookies_by_domain[thirdDomain]);
+
+		/*
+		 * assertions
+		 */
+		test.assert(!!devicesFirst);
+		casper.log('First: ' + devicesFirst.join(), 'debug');
+
+		test.assert(!!devicesThird);
+		casper.log('Third: ' + devicesThird.join(), 'debug');
+
+		test.assertEquals(deviceIdA, devicesFirst[0]); // original deviceId is kept
+		test.assertEquals(devicesFirst[0], devicesThird[0]);
+
+    });
+
+	casper.run(function() {
+		test.done();
+	});
+});
+
+
+casper.test.begin('user_accepts_all | cookies_contain_same_device_ids', 4, function suite(test) {
+
+	phantom.clearCookies();
+
+	var deviceIdA = '1447859209.69696969-6969-6969-6969-696969696969';
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls=1447859209000|v=1|di=' + deviceIdA,
+		'domain': firstDomain,
+	});
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls%3D1447859209000%7Cv%3D1%7Cdi%3D' + deviceIdA,
+		'domain': thirdDomain,
+	});
+
+    casper.start(targetUrl, function() {
+    });
+
+    casper.then(function() {
+		/*
+		 * catch cookies
+		 */
+		var cookies_by_domain = getCookiesByDomain(phantom.cookies);
+
+		// set regexps
+		var reFirst = /[0-9lsv|=]*(di=[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+		var reThird = /.*(di%3D[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+
+		// get device ids
+		var devicesFirst = getDeviceIdsByDomain(reFirst, '=', cookies_by_domain[firstDomain]);
+		var devicesThird = getDeviceIdsByDomain(reThird, '%3D', cookies_by_domain[thirdDomain]);
+
+		/*
+		 * assertions
+		 */
+		test.assert(!!devicesFirst);
+		casper.log('First: ' + devicesFirst.join(), 'debug');
+
+		test.assert(!!devicesThird);
+		casper.log('Third: ' + devicesThird.join(), 'debug');
+
+		test.assertEquals(deviceIdA, devicesFirst[0]); // original deviceId is kept
+		test.assertEquals(devicesFirst[0], devicesThird[0]);
+
+    });
+
+	casper.run(function() {
+		test.done();
+	});
+});
+
+
+casper.test.begin('user_accepts_all | cookies_contain_different_device_ids', 6, function suite(test) {
+
+	phantom.clearCookies();
+	var deviceIdA = '1447859209.11111111-1111-1111-1111-111111111111';
+	var deviceIdB = '1447859209.69696969-6969-6969-6969-696969696969';
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls=1447859209000|v=1|di=' + deviceIdA,
+		'domain': firstDomain,
+	});
+	phantom.addCookie({
+		'name': 'adsp_di',
+		'value': 'ls%3D1447859209000%7Cv%3D1%7Cdi%3D' + deviceIdB,
+		'domain': thirdDomain,
+	});
+
+    casper.start(targetUrl, function() {
+    });
+
+    casper.then(function() {
+		/*
+		 * catch cookies
+		 */
+		var cookies_by_domain = getCookiesByDomain(phantom.cookies);
+
+		// set regexps
+		var reFirst = /[0-9lsv|=]*(di=[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+		var reThird = /.*(di%3D[0-9]+\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})+/g;
+
+		// get device ids
+		var devicesFirst = getDeviceIdsByDomain(reFirst, '=', cookies_by_domain[firstDomain]);
+		var devicesThird = getDeviceIdsByDomain(reThird, '%3D', cookies_by_domain[thirdDomain]);
+
+		/*
+		 * assertions
+		 */
+		test.assert(!!devicesFirst);
+		casper.log('First: ' + devicesFirst.join(), 'debug');
+
+		test.assert(!!devicesThird);
+		casper.log('Third: ' + devicesThird.join(), 'debug');
+
+		test.assertEquals(deviceIdB, devicesFirst[0]); // third deviceId is added to first deviceId
+		test.assertEquals(deviceIdA, devicesFirst[1]); // original deviceId is kept
+		test.assertEquals(deviceIdB, devicesThird[0]); // original deviceId is kept
+
+		test.assertEquals(devicesFirst[0], devicesThird[0]);
 
     });
 
